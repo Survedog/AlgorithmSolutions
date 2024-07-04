68964eb1af9397ab497b2d799b44551a62a0059f
survedog <survedog@gmail.com>
2024-07-04 20:49:01 +0900

    Algospot: Fence 해결 (BruteForce) - https://github.com/Survedog/AlgorithmSolutions/tree/main/Algospot

---
+++ Algospot/Fence_BruteForce.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+#define MAX_H 10000
+#define MAX_N 20000
+
+int C, N;
+int BoardHeights[MAX_N];
+
+int main()
+{
+	cin >> C;
+	for (int Case = 0; Case < C; ++Case)
+	{
+		cin >> N;
+		int MaxSize = 0;
+
+		for (int Board = 0; Board < N; ++Board)
+		{
+			cin >> BoardHeights[Board];
+		}
+
+		for (int Height = 1; Height <= MAX_H; Height++)
+		{
+			int UsableCount = 0;
+			for (int Board = 0; Board < N; ++Board)
+			{
+				if (Height <= BoardHeights[Board])
+				{
+					UsableCount++;
+					MaxSize = max(MaxSize, UsableCount * Height);
+				}
+				else
+				{
+					UsableCount = 0;
+				}
+			}
+		}
+
+		cout << MaxSize << "\n";
+	}
+
+	return 0;
+}
