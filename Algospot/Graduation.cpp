f9e04f47ed1c598f0ab8c4975fc12c2816ccf5bd
survedog <survedog@gmail.com>
2024-08-05 12:33:13 +0900

    Algospot: Graduation 해결 (동적계획법 & 비트마스크) - https://algospot.com/judge/problem/read/GRADUATION

--- Algospot/Graduation.cpp
+++ Algospot/Graduation.cpp
@@ -6,6 +6,12 @@
 * 또한 현재 학기에 수강할 수 있는 과목들의 부분집합을 구할 때는 교재 p.585에서 소개한 비트마스크의 부분집합 순회 방법을 사용했다.
 * 예제는 맞았으나 실제 채점 시 오답이 발생했다.
 * 
+* 책 해설) semester번째 학기 전까지 taken 과목들을 미리 수강했을 때 앞으로 몇 학기를 더 신청해야 K 이상의 과목을 수강할 수 있는지 구하는 함수를 사용한다.
+* 이때 현재 학기에서 수강할 수 있는 과목들의 부분집합을 세기 전, 이미 들었던 과목과 선수과목을 수강하지 않은 과목을 제외해 부분 집합 수를 줄인다.
+* 해당 알고리즘을 사용하니 오답이 발생하지 않았다.
+* 
+* 개선점) 최소 학기 수를 동적계획법으로 바로 구할 수 있는데, 그 방법을 깊게 고민하지 않고 불가능하다 여겨 최대 과목 수를 구하는 간접적인 방법을 대신 사용하였다.
+* 결과적으로 코드가 복잡해지고 알 수 없는 오답에 시달리게 되었다. 앞으론 더 직관적인 알고리즘을 사용할 수 있을지 더 고민해봐야겠다.
 */
 #include <iostream>
 #include <memory.h>
@@ -13,8 +19,9 @@
 
 using namespace std;
 
+const int INF = 987654321;
 int N, K, M, L, prereq[12], open[10];
-int memo[10][1 << 20], prereqSet[1 << 20];
+int memo[10][1 << 12];
 
 inline int GetSubjectCount(int subjects)
 {
@@ -26,32 +33,29 @@ inline int GetLSBSubject(int subjects)
 	return __builtin_ctz(subjects);
 }
 
-int GetPrerequisiteSet(int subjects)
+int GetMinSemesters(int semester, int taken, int takenCount)
 {
-	int& ret = prereqSet[subjects];
-	if (ret != -1) return ret;
-
-	if (subjects == 0) return ret = 0;
-
-	int lsbSubject = GetLSBSubject(subjects);
-	return ret = GetPrerequisiteSet(subjects & (~(1 << lsbSubject))) | prereq[lsbSubject];
-}
-
-int GetMaxSubjects(int semester, int taken, int semesterLeft)
-{
-	if (taken == (1 << N) - 1 || semesterLeft == 0 || semester == M) return 0;
+	if (takenCount == K) return 0;
+	if (semester == M) return INF;
 
 	int& ret = memo[semester][taken];
 	if (ret != -1) return ret;
 
 	int available = open[semester] & (~taken);
+	for (int subject = 0; subject < N; ++subject)
+	{
+		if ((available & (1 << subject)) && (taken & prereq[subject]) != prereq[subject])
+			available ^= 1 << subject;
+	}
+
+	ret = INF;
 	for (int subset = available; subset; subset = ((subset - 1) & available))
 	{
 		int newSubjectCount = GetSubjectCount(subset);
-		if (newSubjectCount > 0 && newSubjectCount <= L && ((prereqSet[subset] & (~taken)) == 0))
-			ret = max(ret, GetMaxSubjects(semester + 1, taken | subset, semesterLeft - 1) + newSubjectCount);
+		if (newSubjectCount <= L)
+			ret = min(ret, GetMinSemesters(semester + 1, taken | subset, takenCount + newSubjectCount) + 1);
 	}
-	ret = max(ret, GetMaxSubjects(semester + 1, taken, semesterLeft));
+	ret = min(ret, GetMinSemesters(semester + 1, taken, takenCount));
 
 	return ret;
 }
@@ -94,25 +98,10 @@ int main()
 			open[i] = subjects;
 		}
 
-		memset(prereqSet, -1, sizeof(prereqSet));
-		for (int subjectSet = (1 << N) - 1; subjectSet >= 0; subjectSet--)
-			GetPrerequisiteSet(subjectSet);
-
-		bool isPossible = false;
-		for (int semesterLeft = 0; semesterLeft <= M; ++semesterLeft)
-		{
-			memset(memo, -1, sizeof(memo));
-
-			if (GetMaxSubjects(0, 0, semesterLeft) >= K)
-			{
-				isPossible = true;
-				cout << semesterLeft << "\n";
-				break;
-			}
-		}
-
-		if (!isPossible)
-			cout << "IMPOSSIBLE" << "\n";
+		memset(memo, -1, sizeof(memo));
+		int result = GetMinSemesters(0, 0, 0);
+		if (result < INF) cout << result << "\n";
+		else cout << "IMPOSSIBLE" << "\n";
 	}
 	return 0;
 }
