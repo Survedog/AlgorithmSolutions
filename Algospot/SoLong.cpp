1f6c6a1c65c448520ac2ad5ab4812135c64947bd
survedog <survedog@gmail.com>
2024-08-20 19:18:41 +0900

    Algospot: SoLong 해결 (트라이) - https://algospot.com/judge/problem/read/SOLONG

--- Algospot/SoLong.cpp
+++ Algospot/SoLong.cpp
@@ -5,27 +5,32 @@
 * 이후 돌고래가 입력한 문자열로 트라이를 타고 내려가며, 각 노드에서 추천되는 간선으로 간 경우와 추천되지 않는 간선으로 간 경우를 따지며
 * typed(이미 입력한 문자 수)와 reserved(자동완성으로 스킵가능한지 아직 모르는 문자의 수)를 조절해 각 문자열 입력에 필요한 최소 타자 수를 계산했다.
 * 예제는 맞았으나 실제 채점에서 오답이 발생함.
+* 
+* 책해답) 사전의 단어들을 벡터에 넣은 뒤 추천순으로 정렬하고 순서대로 트라이에 넣는다. 이렇게하면 트라이의 각 노드에선 자신이 루트인 서브트리 트라이에 가장 먼저 들어온 단어를 추천하면 된다.
+* 이때 메모리와 단어 비교에 필요한 시간을 단축시키기 위해, 추천 단어를 노드에 문자열 그대로 저장하지 않고 벡터에서의 인덱스를 id로 사용해 저장한다.
+* 트라이 노드의 terminal은 int형으로 선언해 현재 노드에서 단어가 끝나면 해당 단어의 id를, 아니면 -1을 저장한다.
+* 이제 이 트라이를 활용해 특정 id의 단어를 치기위해 필요한 최소 타자 수를 구하는 Type 함수를 구현하면 된다.
+* 이때 돌고래가 입력한 단어가 사전에 없다면 해당 단어의 문자수를 통째로 총 타자 수에 더한다.
 */
-
 #include <iostream>
 #include <string>
-#include <memory.h>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int N, M;
 const int ALPHABETS = 26;
+vector<pair<int, string>> words;
 
 int toNumber(char ch) { return ch - 'A'; }
 
 struct TrieNode
 {
-	char autoCompNext;
-	int freq, autoCompFreq;
-	bool isTerminal;
+	int autoComp, terminal;
 	TrieNode* children[ALPHABETS];
 
-	TrieNode() : isTerminal(false), freq(0), autoCompFreq(0), autoCompNext(0)
+	TrieNode() : terminal(-1), autoComp(-1)
 	{
 		fill(&children[0], &children[ALPHABETS], nullptr);
 	}
@@ -37,46 +42,37 @@ struct TrieNode
 				delete child;
 	}
 
-	void Insert(const char* key, int inFreq)
+	void Insert(const char* key, int id)
 	{
-		if (inFreq > autoCompFreq || (inFreq == autoCompFreq && *key <= autoCompNext))
-		{
-			autoCompFreq = inFreq;
-			autoCompNext = *key;
-		}
+		if (autoComp == -1) autoComp = id;
 
 		if (*key == 0)
-		{
-			freq = inFreq;
-			isTerminal = true;
-		}
+			terminal = id;
 		else
 		{
 			int nextNum = toNumber(*key);
 			TrieNode* next = children[nextNum];
 			if (next == nullptr)
 				next = children[nextNum] = new TrieNode();
-			next->Insert(key + 1, inFreq);
+			next->Insert(key + 1, id);
 		}
 	}
 
-	int FindTypeCount(const char* key, int wordLenLeft, int typed, int reserved)
+	TrieNode* Find(const char* key)
 	{
-		if (*key == 0)
-		{
-			if (reserved == 0) return typed;
-			return autoCompNext == 0 ? typed + 1 : typed + reserved;
-		}
+		if (*key == 0) return this;
 
-		if (autoCompNext == *key)
-			return children[toNumber(autoCompNext)]->FindTypeCount(key + 1, wordLenLeft - 1, typed, reserved + 1);
-		else
-		{
-			if (children[toNumber(*key)])
-				return children[toNumber(*key)]->FindTypeCount(key + 1, wordLenLeft - 1, typed + reserved + 1, 0);
-			else
-				return wordLenLeft + typed + reserved;
-		}
+		TrieNode* next = children[toNumber(*key)];
+		if (next == nullptr) return nullptr;
+		return next->Find(key + 1);
+	}
+
+	int Type(const char* key, int id)
+	{
+		if (*key == 0) return 0;
+		if (autoComp == id) return 1;
+		TrieNode* next = children[toNumber(*key)];
+		return next->Type(key + 1, id) + 1;
 	}
 };
 
@@ -90,22 +86,33 @@ int main()
 	while (C--)
 	{
 		cin >> N >> M;
-		TrieNode* trieRoot = new TrieNode();
-		trieRoot->autoCompFreq = 11;
+
+		words.clear();
+		words.reserve(N);
 		for (int i = 0; i < N; ++i)
 		{
 			string word;
 			int freq;
 			cin >> word >> freq;
-			trieRoot->Insert(word.c_str(), freq);
+			words.emplace_back(-freq, word);
 		}
+		sort(words.begin(), words.end());
+
+		TrieNode* trieRoot = new TrieNode();
+		for (int i = 0; i < words.size(); ++i)
+			trieRoot->Insert(words[i].second.c_str(), i);
+		trieRoot->autoComp = -1;
 
 		int sum = M - 1;
 		for (int i = 0; i < M; ++i)
 		{
 			string word;
 			cin >> word;
-			sum += trieRoot->FindTypeCount(word.c_str(), word.length(), 0, 0);
+			TrieNode* node = trieRoot->Find(word.c_str());
+			if (node && node->terminal != -1)
+				sum += trieRoot->Type(word.c_str(), node->terminal);
+			else
+				sum += word.size();
 		}
 		cout << sum << "\n";
 		delete trieRoot;
