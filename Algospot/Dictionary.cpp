2bb3890b2290b683dc17cb002842d7b3298943df
survedog <survedog@gmail.com>
2024-08-21 22:59:18 +0900

    Dictionary 풀이에 최적화 적용

--- Algospot/Dictionary.cpp
+++ Algospot/Dictionary.cpp
@@ -1,8 +1,11 @@
 /*
 * Problem: https://algospot.com/judge/problem/read/DICTIONARY
-* 문자열들을 서로 비교하며 개별 문자 간 순서를 알아내 adjacent에 저장하고, 이를 이용해 위상정렬로 알파벳을 순서에 맞게 정렬했다.
+* 문자열(단어)들을 서로 비교하며 개별 문자 간 순서를 알아내 adjacent에 저장하고, 이를 이용해 위상정렬로 알파벳을 순서에 맞게 정렬했다.
 * 이때 위상 정렬은 DFS를 사용했으며(교재 831페이지 증명 참고), 마지막에 정렬된 순서를 뒤집는 수고를 덜기 위해 처음부터 문자 간 순서를 반대로 저장했다.
 * 또한 모순이 발생하는지 찾기 위해 결과 문자열을 순회하며 현재 문자의 adjacent가 자신의 앞에 등장하는지 살펴보며 개별 순서가 모두 만족되는지 확인하였다.
+* 
+* 보충) 책 해설을 읽은 결과, 단어들을 비교할 때 모든 단어 쌍에 대해 할 필요 없이 인접한 단어끼리만 비교해도 필요한 모든 정보를 얻을 수 있다는 것을 알 수 있었다.
+* 이를 적용한 결과 수행 시간이 기존의 172ms에서 4ms로 크게 단축되었다.
 */
 #include <iostream>
 #include <string>
@@ -60,9 +63,11 @@ int main()
 		for (int i = 0; i < 26; ++i)
 			adjacent[i].clear();
 
-		for (int i = 0; i < n; ++i)
-			for (int j = i + 1; j < n; ++j)
-				CheckAdjacent(words[i], words[j]);
+		for (int i = 0; i < n - 1; ++i)
+		{
+			int j = i + 1;
+			CheckAdjacent(words[i], words[j]);
+		}
 
 		answer.clear();
 		answer.reserve(26);
