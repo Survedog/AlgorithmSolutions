02d7037c71379dbdb718d398425ef630277bd11e
survedog <survedog@gmail.com>
2024-08-21 22:49:39 +0900

    DIctionary 설명 보충 및 코드 일부 변경

--- Algospot/Dictionary.cpp
+++ Algospot/Dictionary.cpp
@@ -2,19 +2,18 @@
 * Problem: https://algospot.com/judge/problem/read/DICTIONARY
 * 문자열들을 서로 비교하며 개별 문자 간 순서를 알아내 adjacent에 저장하고, 이를 이용해 위상정렬로 알파벳을 순서에 맞게 정렬했다.
 * 이때 위상 정렬은 DFS를 사용했으며(교재 831페이지 증명 참고), 마지막에 정렬된 순서를 뒤집는 수고를 덜기 위해 처음부터 문자 간 순서를 반대로 저장했다.
+* 또한 모순이 발생하는지 찾기 위해 결과 문자열을 순회하며 현재 문자의 adjacent가 자신의 앞에 등장하는지 살펴보며 개별 순서가 모두 만족되는지 확인하였다.
 */
 #include <iostream>
 #include <string>
 #include <vector>
-#include <set>
 #include <memory.h>
 
 using namespace std;
 
 int n;
 string words[1000];
-vector<char> answer;
-set<char> adjacent[26];
+vector<char> answer, adjacent[26];
 bool visited[26];
 
 inline int ToNumber(char ch) { return ch - 'a'; }
@@ -28,7 +27,7 @@ void CheckAdjacent(const string& a, const string& b)
 	}
 
 	if (idxA < a.size() && idxB < b.size())
-		adjacent[ToNumber(b[idxB])].insert(a[idxA]);
+		adjacent[ToNumber(b[idxB])].push_back(a[idxA]);
 }
 
 void DFS(char ch)
