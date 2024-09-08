d36ecef31eb7291027879d66e2e8bcc208c61ed6
survedog <survedog@gmail.com>
2024-09-08 23:59:17 +0900

    TrapCard 각 노드를 식별하는 방법을 좌표에서 ID로 변경

--- Algospot/TrapCard.cpp
+++ Algospot/TrapCard.cpp
@@ -13,49 +13,134 @@ const int MAX_N = 200, MAX_M = 200;
 const int dx[] = { 1, -1, 0, 0 }, dy[] = { 0, 0, 1, -1 };
 
 int H, W, n, m;
+bool adjacent[MAX_N][MAX_M];
 vector<string> board;
-vector<vector<bool>> visited;
-vector<vector<pair<int, int>>> aMatch, bMatch;
+vector<bool> visited;
+vector<int> aMatch, bMatch;
+vector<vector<int>> blockID;
+vector<pair<int, int>> aID2Pos, bID2Pos;
+
+inline int GetTeam(int y, int x) { return (x + y) % 2; }
+
+int AssignID(int y, int x)
+{
+	if (GetTeam(y, x) == 0)
+	{
+		blockID[y][x] = n++;
+		aID2Pos[blockID[y][x]] = make_pair(y, x);
+	}
+	else
+	{
+		blockID[y][x] = m++;
+		bID2Pos[blockID[y][x]] = make_pair(y, x);
+	}
+	return blockID[y][x];
+}
 
 inline bool IsInRange(int y, int x)
 {
 	return y >= 0 && y < H && x >= 0 && x < W;
 }
 
-bool DFS(int y, int x)
+bool DFS(int a)
 {
-	if (visited[y][x]) return false;
-	visited[y][x] = true;
+	if (visited[a]) return false;
+	visited[a] = true;
 
-	for (int i = 0; i < 4; ++i)
+	for (int b = 0; b < m; ++b)
 	{
-		int ny = y + dy[i];
-		int nx = x + dx[i];
+		if (!adjacent[a][b]) continue;
 
-		if (!IsInRange(ny, nx)) continue;
-
-		int cy = bMatch[ny][nx].first;
-		int cx = bMatch[ny][nx].second;
-		if (aMatch[ny][nx] == make_pair(-1, -1) || DFS(cy, cx))
+		if (bMatch[b] == -1 || DFS(bMatch[b]))
 		{
-			aMatch[ny][nx] = make_pair(y, x);
-			bMatch[]
+			aMatch[a] = b;
+			bMatch[b] = a;
+			return true;
 		}
 	}
+	return false;
 }
 
 int BipartiteMatch()
 {
-	aMatch = bMatch = vector<vector<pair<int, int>>>(H, vector<pair<int, int>>(W, make_pair(-1, -1)));
+	aMatch = vector<int>(n, -1);
+	bMatch = vector<int>(m, -1);
 
 	int match = 0;
-	for (int i = 0; i < H; ++i)
-		for (int j = 0; j < W; ++j)
-		{
-			visited = vector<vector<bool>>(H, vector<bool>(W, false));
-			if (DFS(i, j)) match++;
-		}
+	for (int i = 0; i < n; ++i)
+	{
+		visited = vector<bool>(n, false);
+		if (DFS(i)) match++;
+	}
 	return match;
+}
+
+void MakeGraph()
+{
+	n = 0; m = 0;
+	blockID = vector<vector<int>>(H, vector<int>(W, -1));
+	aID2Pos = bID2Pos = vector<pair<int, int>>(H * W / 2);
+	memset(adjacent, false, sizeof(adjacent));
+
+	for (int y = 0; y < H; ++y)
+		for (int x = 0; x < W; ++x)
+		{
+			if (board[y][x] != '.') continue;
+
+			if (blockID[y][x] == -1) blockID[y][x] = AssignID(y, x);
+			int id = blockID[y][x];
+
+			for (int dir = 0; dir < 4; ++dir)
+			{
+				int cy = y + dy[dir];
+				int cx = x + dx[dir];
+
+				if (blockID[cy][cx] == -1) blockID[cy][cx] = AssignID(cy, cx);
+				int cID = blockID[cy][cx];
+
+				if (IsInRange(cy, cx) && board[cy][cx] == '.')
+					adjacent[id][cID] = adjacent[cID][id] = true;
+			}
+		}
+}
+
+int GetAnswer()
+{
+	MakeGraph();
+	BipartiteMatch();
+
+	vector<bool> aSelected(n, true), bSelected(m, false);
+
+	int ret = n;
+	for (int i = 0; i < m; ++i)
+		if (bMatch[i] == -1)
+		{
+			bSelected[i] = true;
+			ret++;
+		}
+
+	for (int a = 0; a < n; ++a)
+		for (int b = 0; b < m; ++b)
+		{
+			if (aMatch[a] != b && adjacent[a][b])
+			{
+				aSelected[a] = false;
+				bSelected[b] = true;
+			}
+
+			while ()
+			{
+
+			}
+		}
+
+	for (int a = 0; a < n; ++a)
+		if (aSelected[a])
+		{
+
+		}
+
+	return ret;
 }
 
 int main()
@@ -71,6 +156,9 @@ int main()
 		board = vector<string>(H);
 		for (int i = 0; i < H; ++i)
 			cin >> board[i];
+
+		GetAnswer();
 	}
 	return 0;
 }
+
