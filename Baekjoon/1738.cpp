ad29cdbd0f602be11365cf9f4d9ff7bc7d420634
survedog <survedog@gmail.com>
2024-09-15 23:59:46 +0900

    [백준]1738 - 골목길 미해결 (벨만-포드) - https://www.acmicpc.net/problem/1738

---
+++ Baekjoon/1738.cpp
@@ -0,0 +1,80 @@
+﻿/*
+* Problem: https://www.acmicpc.net/problem/1738
+* 분류: 벨만 포드 알고리즘
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+const int INF = 987654321;
+int N, M;
+vector<vector<int>> weight;
+
+vector<int> BellmanFord()
+{
+	vector<int> dist(N + 1, INF);
+	vector<int> parent(N + 1, -1);
+	dist[1] = 0;
+
+	for (int iter = 0; iter < N - 1; ++iter)
+	{
+		bool updated = false;
+		for (int u = 1; u <= N; ++u)
+			for (int v = 1; v <= N; ++v)
+				if (weight[u][v] != INF && dist[v] > dist[u] + weight[u][v])
+				{
+					dist[v] = dist[u] + weight[u][v];
+					updated = true;
+					parent[v] = u;
+				}
+
+		if (!updated) break;
+	}
+
+	for (int u = 1; u <= N; ++u)
+		for (int v = 1; v <= N; ++v)
+			if (weight[u][v] != INF && dist[v] > dist[u] + weight[u][v])
+				return vector<int>();
+
+	vector<int> path;
+	int node = N;
+	while (node != -1)
+	{
+		path.push_back(node);
+		node = parent[node];
+	}
+
+	if (path.back() != 1)
+		return vector<int>();
+
+	reverse(path.begin(), path.end());
+	return path;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N >> M;
+	weight = vector<vector<int>>(N + 1, vector<int>(N + 1, INF));
+	for (int i = 0; i < M; ++i)
+	{
+		int u, v, w;
+		cin >> u >> v >> w;
+		weight[u][v] = min(weight[u][v], -w);
+	}
+
+	vector<int> Path = BellmanFord();
+	if (Path.empty())
+		cout << "-1\n";
+	else
+	{
+		for (int node : Path)
+			cout << node << " ";
+		cout << "\n";
+	}
+	return 0;
+}
