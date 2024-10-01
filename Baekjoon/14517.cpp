aa1aec0fb32ccb4f171d31f256d24f5e9a9bbc4c
survedog <survedog@gmail.com>
2024-10-01 23:57:34 +0900

    [백준]14517 - 팰린드롬 개수 구하기 (Large) 미해결 (동적계획법) - https://www.acmicpc.net/problem/14517

---
+++ Baekjoon/14517.cpp
@@ -0,0 +1,48 @@
+﻿/*
+* Problem: https://www.acmicpc.net/problem/14517
+* 분류: 동적계획법
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+const int DIVIDER = 10007;
+vector<int> matches[1000];
+vector<vector<int>> memo;
+string S;
+
+int CountPalindromes()
+{
+	memo[0][0] = 1;
+	for (int i = 1; i < S.size(); ++i)
+	{
+		memo[i][i] = 1;
+		memo[i - 1][i] = S[i - 1] == S[i] ? 3 : 2;
+	}
+
+	for (int len = 3; len <= S.size(); ++len)
+	{
+		for (int i = 0; i <= S.size() - len; ++i)
+		{
+			int j = i + len - 1;
+			if (S[i] == S[j])
+				memo[i][j] = (memo[i + 1][j] + memo[i][j - 1] + 1) % DIVIDER;
+			else
+				memo[i][j] = (memo[i + 1][j] + memo[i][j - 1] - memo[i + 1][j - 1]) % DIVIDER;
+		}
+	}
+	return memo[0][S.size() - 1];
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> S;
+	memo.resize(S.size(), vector<int>(S.size(), -1));
+	cout << CountPalindromes() << "\n";
+	return 0;
+}
